5a020a098302c63ac1249eec877a95f847805418
Juan Batty <jmbl1685@hotmail.com>
2018-09-10 15:16:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const app        = express()
 const multer     = require('multer')
 const handlefile = require('./file_handle')
 
-/* You cant use dotenv library*/
+/* You cant use dotenv library */
 const port = process.env.PORT || 3000
-
-app.use(express.static('storage_files'))
 
 const upload = multer({
   storage: multer.memoryStorage()
